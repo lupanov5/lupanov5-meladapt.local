47ff1291697a0734e31ee7ddff4e20dfcea4e11a
Lupanov Dmitry <lupanov5@mail.ru>
2022-03-28 13:39:32 +0300

    Добавляет ссылки на магазины

--- local/templates/meladapt/blocks/buy.php
+++ local/templates/meladapt/blocks/buy.php
@@ -8,52 +8,68 @@
 
         <ul class="buy__list list" data-load-content>
             <li class="list__item list__item_shop">
-                <div class="list__head list__head_shop">
-                    <img src="/local/templates/meladapt/assets/images/shop-1.png" alt="shop">
-                </div>
-                <div class="list__title list__title_shop">Apteka.ru</div>
+                <a href="#" class="list__item_link">
+                    <div class="list__head list__head_shop">
+                        <img src="/local/templates/meladapt/assets/images/shop-1.png" alt="shop">
+                    </div>
+                    <div class="list__title list__title_shop">Apteka.ru</div>
+                </a>
             </li>
             <li class="list__item list__item_shop">
-                <div class="list__head list__head_shop">
-                    <img src="/local/templates/meladapt/assets/images/shop-2.png" alt="shop">
-                </div>
-                <div class="list__title list__title_shop">Сбер Еаптека</div>
+                <a href="#"  class="list__item_link">
+                    <div class="list__head list__head_shop">
+                        <img src="/local/templates/meladapt/assets/images/shop-2.png" alt="shop">
+                    </div>
+                    <div class="list__title list__title_shop">Сбер Еаптека</div>
+                </a>
             </li>
             <li class="list__item list__item_shop">
-                <div class="list__head list__head_shop">
-                    <img src="/local/templates/meladapt/assets/images/shop-3.png" alt="shop">
-                </div>
-                <div class="list__title list__title_shop">Яндекс.Маркет</div>
+                <a href="#"  class="list__item_link">
+                    <div class="list__head list__head_shop">
+                        <img src="/local/templates/meladapt/assets/images/shop-3.png" alt="shop">
+                    </div>
+                    <div class="list__title list__title_shop">Яндекс.Маркет</div>
+                </a>
             </li>
             <li class="list__item list__item_shop">
-                <div class="list__head list__head_shop">
-                    <img src="/local/templates/meladapt/assets/images/shop-4.png" alt="shop">
-                </div>
-                <div class="list__title list__title_shop">Вита</div>
+                <a href="#"  class="list__item_link">
+                    <div class="list__head list__head_shop">
+                        <img src="/local/templates/meladapt/assets/images/shop-4.png" alt="shop">
+                    </div>
+                    <div class="list__title list__title_shop">Вита</div>
+                </a>
             </li>
             <li class="list__item list__item_shop">
-                <div class="list__head list__head_shop">
-                    <img src="/local/templates/meladapt/assets/images/shop-5.png" alt="shop">
-                </div>
-                <div class="list__title list__title_shop">Планета здоровья</div>
+                <a href="#"  class="list__item_link">
+                    <div class="list__head list__head_shop">
+                        <img src="/local/templates/meladapt/assets/images/shop-5.png" alt="shop">
+                    </div>
+                    <div class="list__title list__title_shop">Планета здоровья</div>
+                </a>
             </li>
             <li class="list__item list__item_shop">
-                <div class="list__head list__head_shop">
-                    <img src="/local/templates/meladapt/assets/images/shop-6.png" alt="shop">
-                </div>
-                <div class="list__title list__title_shop">Uteka.ru</div>
+                <a href="#"  class="list__item_link">
+                    <div class="list__head list__head_shop">
+                        <img src="/local/templates/meladapt/assets/images/shop-6.png" alt="shop">
+                    </div>
+                    <div class="list__title list__title_shop">Uteka.ru</div>
+                </a>
             </li>
             <li class="list__item list__item_shop">
-                <div class="list__head list__head_shop">
-                    <img src="/local/templates/meladapt/assets/images/shop-7.png" alt="shop">
-                </div>
-                <div class="list__title list__title_shop">Asna.ru</div>
+                <a href="#"  class="list__item_link">
+                    <div class="list__head list__head_shop">
+                        <img src="/local/templates/meladapt/assets/images/shop-7.png" alt="shop">
+                    </div>
+                    <div class="list__title list__title_shop">Asna.ru</div>
+                </a>
             </li>
             <li class="list__item list__item_shop">
-                <div class="list__head list__head_shop">
-                    <img src="/local/templates/meladapt/assets/images/shop-8.png" alt="shop">
-                </div>
-                <div class="list__title list__title_shop">Магнит-Аптека</div>
+                <a href="#"  class="list__item_link">
+                    <div class="list__head list__head_shop">
+                        <img src="/local/templates/meladapt/assets/images/shop-8.png" alt="shop">
+                    </div>
+                    <div class="list__title list__title_shop">Магнит-Аптека</div>
+                </a>
             </li>
         </ul>
         <a href="" class="link buy__show" data-show-more="1" data-show-more-url="/request/"><span>Показать все</span></a>
