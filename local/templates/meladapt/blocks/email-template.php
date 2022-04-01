a236596f7ace3b53ba3dbf0f9c0ea7f4df1097dd
LupanovDmitry <lupanov5@mailru>
2022-04-01 11:55:38 +0300

    Добавляет Почтовый шаблон

--- local/templates/meladapt/blocks/email-template.php
+++ local/templates/meladapt/blocks/email-template.php
@@ -1,59 +1,35 @@
-<!DOCTYPE HTML>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<div class="container">
+    <section class="email">
+        <div class="email__img" style="background-image: url('/local/templates/meladapt/assets/images/bg.jpg')">
+            <a href="/" class="brand">
+                <img src="/local/templates/meladapt/assets/images/logo.png""
+                     class="brand__img">
+            </a>
 
-    <meta name="author" content="Контент">
-    <meta name="keywords" content="keywords">
-    <meta name="description" content="description">
-
-    <title>Меладапт</title>
-
-    <link rel="icon" type="image/png" href="/local/templates/meladapt/favicon.png">
-    <link rel="stylesheet" href="/local/templates/meladapt/assets/vendor.css">
-    <link rel="stylesheet" href="/local/templates/meladapt/assets/app.min.css">
-
-    <meta property="og:title" content="title"/>
-    <meta property="og:description" content="description"/>
-    <meta property="og:image" content="favicon.png">
-
-</head>
-<body>
-
-    <div class="container">
-        <section class="email">
-            <div class="email__img" style="background-image: url('/local/templates/meladapt/assets/images/bg.jpg')">
-                <a href="/" class="brand">
-                    <img src="/local/templates/meladapt/assets/images/logo.png"
-                         alt="{$smarty.const.SITE_NAME}"
-                         class="brand__img">
-                </a>
-
-                <div class="promo__product">
-                    <div class="inner">
-                        <img src="/local/templates/meladapt/assets/images/product.png" alt="">
-                    </div>
+            <div class="promo__product">
+                <div class="inner">
+                    <img src="/local/templates/meladapt/assets/images/product.png" alt="">
                 </div>
             </div>
+        </div>
 
-            <h2 class="email__title">
-                Ваш вопрос принят
-            </h2>
-            <div class="email__text">
-                Блок текста с основным посылом рассылки, отвечающий на вопрос что произошло и что делать дальше. Например, о том, что теперь надо ждать звонок менеджера, который позвонит в течение
-                24 часов
-            </div>
+        <h2 class="email__title">
+            Ваш вопрос принят
+        </h2>
+        <div class="email__text">
+            Блок текста с основным посылом рассылки, отвечающий на вопрос что произошло и что делать дальше. Например, о том, что теперь надо ждать звонок менеджера, который позвонит в течение
+            24 часов
+        </div>
 
+        <div class="email__btn">
+            <a class="btn btn_lt">Вернуться на сайт</a>
+        </div>
 
+        <div class="email__bottom">
+            <a href="#" class="email__bottom_item"><span>Отписаться от рассылки</span></a>
+            <div class="email__bottom_item" >© Меладапт</div>
+            <div class="email__bottom_item" >18+</div>
+        </div>
 
-        </section>
-    </div>
-
-
-
-
-<script src="/local/templates/meladapt/assets/app.min.js"></script>
-</body>
-</html>
+    </section>
+</div>
